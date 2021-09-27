ef32fe42d4a2c07e8967b3586a8ac563a250f4a9
Seb James <seb.james@sheffield.ac.uk>
2021-09-27 22:18:39 +0100

    adds a new avx2 example

---
+++ avx2_longvector.c
@@ -0,0 +1,26 @@
+/*
+ * Construct a 256-bit vector from 4 64-bit doubles. Add it to itself and print the
+ * result.
+ */
+
+#include <stdio.h>
+#include <immintrin.h>
+
+int main()
+{
+    __attribute__ ((aligned (32))) float float_array[16] = {100, 200, 300, 400, 500, 600, 700, 800, 101, 201, 301, 401, 501, 601, 701, 801};
+
+    __m256 fv = _mm256_load_ps (float_array);
+    __m256 fv2 = _mm256_load_ps (float_array+8);
+    __m256 two = _mm256_set1_ps (2.0f);
+
+    __m256 c = _mm256_mul_ps(fv, two);
+    __m256 c2 = _mm256_mul_ps(fv2, two);
+
+    __attribute__ ((aligned (32))) float output[16];
+    _mm256_store_ps(output, c);
+    _mm256_store_ps(output+8, c2);
+
+    for (int i = 0; i < 16; ++i) { printf("%f\n", output[i]); }
+    return 0;
+}
