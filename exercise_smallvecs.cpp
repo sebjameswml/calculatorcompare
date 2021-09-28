716a0784eef1ebe0eaad212e3ace57b021bf5d96
Seb James <seb.james@sheffield.ac.uk>
2021-09-28 19:36:10 +0100

    Where I discover that Eigen is fast on smaller vectors

--- exercise_smallvecs.cpp
+++ exercise_smallvecs.cpp
@@ -1,3 +1,8 @@
+/*
+ * Like exercise.cpp but on small vVectors which might make vVector's OpenMP slow due to
+ * start up costs.
+ */
+
 #include <iostream>
 #include <Eigen/Dense>
 #include <morph/Random.h>
@@ -11,6 +16,9 @@ using std::chrono::steady_clock;
 typedef float F;
 typedef Eigen::Array<F, Eigen::Dynamic, 1> EigenVec;
 
+// How long are the vectors?
+const size_t veclen = 4;
+
 int main()
 {
     morph::RandUniform<float> rng;
@@ -18,9 +26,9 @@ int main()
     steady_clock::duration sincestart;
 
     // Multiplication. Eigen
-    EigenVec ev(1000000);
+    EigenVec ev(veclen);
     for (auto& vv : ev) { vv = rng.get(); }
-    EigenVec ev2(1000000);
+    EigenVec ev2(veclen);
 
     start = steady_clock::now();
     for (F i = F{0}; i < F{500}; i += F{1}) {
@@ -28,25 +36,25 @@ int main()
     }
     sincestart = steady_clock::now() - start;
 
-    std::cout << "Scalar mult took " << duration_cast<milliseconds>(sincestart).count() << " ms with Eigen" << std::endl;
+    std::cout << "Scalar mult took " << duration_cast<nanoseconds>(sincestart).count() << " ns with Eigen" << std::endl;
 
     // Multiplication. vVector
-    morph::vVector<F> v(1000000);
+    morph::vVector<F> v(veclen);
     v.randomize();
-    morph::vVector<F> v2(1000000);
+    morph::vVector<F> v2(veclen);
 
     start = steady_clock::now();
     for (F i = F{0}; i < F{500}; i += F{1}) {
-        //v *= i; // super fast. 500 million mults in 8 ms = 62.5 gflops (with omp parallel)
-        //v2 = v * i; // Same speed as Eigen (but with omp parallel). 65 ms.
-        v.mult (i, v2); // With OpenMP parallel on an i9, this whips Eigen (20ms to 66 ms).
+        //v *= i; // super fast. 500 million mults in 8 ns = 62.5 gflops (with omp parallel)
+        //v2 = v * i; // Same speed as Eigen (but with omp parallel). 65 ns.
+        v.mult (i, v2); // With OpenMP parallel on an i9, this whips Eigen (20ns to 66 ns).
     }
     sincestart = steady_clock::now() - start;
 
-    std::cout << "Scalar mult took " << duration_cast<milliseconds>(sincestart).count() << " ms with vVector" << std::endl;
+    std::cout << "Scalar mult took " << duration_cast<nanoseconds>(sincestart).count() << " ns with vVector" << std::endl;
 
     // Vec Multiplication. Eigen
-    EigenVec ev3(1000000);
+    EigenVec ev3(veclen);
     for (auto& vv : ev3) { vv = rng.get(); }
 
     start = steady_clock::now();
@@ -55,10 +63,10 @@ int main()
     }
     sincestart = steady_clock::now() - start;
 
-    std::cout << "Vector mult took " << duration_cast<milliseconds>(sincestart).count() << " ms with Eigen" << std::endl;
+    std::cout << "Vector mult took " << duration_cast<nanoseconds>(sincestart).count() << " ns with Eigen" << std::endl;
 
     // Vec Multiplication. vVector
-    morph::vVector<F> v3(1000000);
+    morph::vVector<F> v3(veclen);
     v3.randomize();
 
     start = steady_clock::now();
@@ -67,7 +75,7 @@ int main()
     }
     sincestart = steady_clock::now() - start;
 
-    std::cout << "Vector mult took " << duration_cast<milliseconds>(sincestart).count() << " ms with vVector" << std::endl;
+    std::cout << "Vector mult took " << duration_cast<nanoseconds>(sincestart).count() << " ns with vVector" << std::endl;
 
     // Vec Division. Eigen
     start = steady_clock::now();
@@ -76,7 +84,7 @@ int main()
     }
     sincestart = steady_clock::now() - start;
 
-    std::cout << "Vector div took " << duration_cast<milliseconds>(sincestart).count() << " ms with Eigen" << std::endl;
+    std::cout << "Vector div took " << duration_cast<nanoseconds>(sincestart).count() << " ns with Eigen" << std::endl;
 
     // Vec Division. vVector
     start = steady_clock::now();
@@ -85,23 +93,23 @@ int main()
     }
     sincestart = steady_clock::now() - start;
 
-    std::cout << "Vector div took " << duration_cast<milliseconds>(sincestart).count() << " ms with vVector" << std::endl;
+    std::cout << "Vector div took " << duration_cast<nanoseconds>(sincestart).count() << " ns with vVector" << std::endl;
 
     // Pow
     start = steady_clock::now();
     for (F i = F{0}; i < F{500}; i += F{1}) {
-        ev = ev2.pow(F{1}/i); // 2640 ms
+        ev = ev2.pow(F{1}/i); // 2640 ns
     }
     sincestart = steady_clock::now() - start;
-    std::cout << "Raise to scalar power took " << duration_cast<milliseconds>(sincestart).count() << " ms with Eigen" << std::endl;
+    std::cout << "Raise to scalar power took " << duration_cast<nanoseconds>(sincestart).count() << " ns with Eigen" << std::endl;
 
     start = steady_clock::now();
     for (F i = F{0}; i < F{500}; i += F{1}) {
-        //v.pow_inplace (F{1}/i); // 949 ms no OMP, 120 ms with.
-        v2 = v.pow (F{1}/i); // 2617 ms. 320 ms with.
+        //v.pow_inplace (F{1}/i); // 949 ns no OMP, 120 ns with.
+        v2 = v.pow (F{1}/i); // 2617 ns. 320 ns with.
     }
     sincestart = steady_clock::now() - start;
-    std::cout << "Raise to scalar power took " << duration_cast<milliseconds>(sincestart).count() << " ms with vVector" << std::endl;
+    std::cout << "Raise to scalar power took " << duration_cast<nanoseconds>(sincestart).count() << " ns with vVector" << std::endl;
 
     return 0;
 }
