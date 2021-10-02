2717557a1ad6edfc66da423c07a1153b011c3ed2
Seb James <seb.james@sheffield.ac.uk>
2021-10-02 15:05:36 +0100

    Update exerciseEigen to match work done in avx2_vVector

--- exerciseEigen.cpp
+++ exerciseEigen.cpp
@@ -6,22 +6,24 @@
 using namespace std::chrono;
 using std::chrono::steady_clock;
 
+typedef Eigen::Array<float, Eigen::Dynamic, 1> EigenVec;
+
 int main()
 {
     morph::RandUniform<float> rng;
-    Eigen::VectorXf v(1000000);
+    EigenVec v(8192);
     for (auto& vv : v) { vv = rng.get(); }
+    EigenVec v1(8192);
+    float i = 0.0f;
+    for (auto& vv : v1) { vv = i; i += 1.0f; }
+
+    EigenVec v2(8192);
 
     steady_clock::time_point start = steady_clock::now();
-
-    Eigen::VectorXf v2(1000000);
-    for (float i = 0.0f; i < 500.0f; i += 1.0f) {
-        v2 = v * i;
-    }
-
+    v2 = v * 2.0f;
     steady_clock::duration sincestart = steady_clock::now() - start;
 
-    std::cout << "Operation took " << duration_cast<milliseconds>(sincestart).count() << " ms" << std::endl;
+    std::cout << "Operation took " << duration_cast<nanoseconds>(sincestart).count() << " ns" << std::endl;
 
     return 0;
 }
